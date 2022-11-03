4acf0f58b5634c3bf780f0cccdb4b62c3d63aff3
yiweixiangni <87159120+yiweixiangni@users.noreply.github.com>
2022-11-03 19:12:58 +0800

    Update GitHubController.java

--- src/main/java/cn/nun/controller/GitHubController.java
+++ src/main/java/cn/nun/controller/GitHubController.java
@@ -3,7 +3,7 @@ package cn.nun.controller;
 public class GitHubController {
 
     public static void main(String[] args) {
-        System.out.println("余小达");
+        System.out.println("常泽");
     }
 
 }
